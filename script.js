9469e2f9671148e803886c2062db124640b84571
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2026-05-26 17:01:45 +0800

    Refactor grid generation

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const container = document.querySelector(".container");
 const gridButton = document.querySelector(".gridBtn");
 
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
-}
-
-container.addEventListener("mouseover", (e) => {
-  // Prevent container from blacking out on mouseover
-  if (e.target !== e.currentTarget) {
-    e.target.style.backgroundColor = "black";
-    setTimeout(() => {
-      e.target.style.backgroundColor = "";
-    }, 500);
+function generateGrid(len) {
+  const grid = document.createElement("div");
+  for (let i = 0; i < Math.pow(len, 2); i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square);
   }
-});
+  grid.addEventListener("mouseover", (e) => {
+    // Prevent container from blacking out on mouseover
+    if (e.target !== e.currentTarget) {
+      e.target.style.backgroundColor = "black";
+      setTimeout(() => {
+        e.target.style.backgroundColor = "";
+      }, 500);
+    }
+  });
+  container.appendChild(grid);
+}
 
 gridButton.addEventListener("click", () => {
   let size = prompt("How many squares across should the new grid be?");
@@ -23,3 +26,5 @@ gridButton.addEventListener("click", () => {
     alert("Whoa, a grid that size is too big to handle!");
   }
 });
+
+generateGrid(16);
