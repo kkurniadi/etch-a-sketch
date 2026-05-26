7aca01bc9666e6f148c84a4175e720dc3e7701af
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2026-05-26 16:01:58 +0800

    Remove row divs

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    row.appendChild(square);
-  }
-  container.appendChild(row);
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  container.appendChild(square);
 }
